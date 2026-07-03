161886448e571bea068c0ced66d5ac7eb1f1242d
LovableinTrouble <slinkingbox@Outlook.com>
2026-07-03 09:12:46 -0400

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -20,7 +20,6 @@ import { BootLoader } from "../components/BootLoader";
 import { BottomNav } from "../components/BottomNav";
 import { LogoWord } from "../components/Logo";
 import { SharePopup } from "../components/SharePopup";
-import { MusicMiniPlayer } from "../components/MusicMiniPlayer";
 import { useSettings } from "../lib/store";
 
 function NotFoundComponent() {
